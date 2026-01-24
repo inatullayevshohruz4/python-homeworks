74e027fa410e7f68a3c29242639520946ddf4599
inatullayevshohruz4 <inatullayevshohruz4@gmail.com>
2026-01-24 13:00:58 +0500

    homework1

---
+++ lesson-1/lesson-1.py
@@ -0,0 +1,16 @@
+#1. Why do you think companies analyze large volumes of data?
+#Fist of all these companies have a huge amount of data and they need that data analytics, with that they can forecast future risks and they work along with those risks to get the least lost.
+#Especially in a wast data managers might be messed up. So avoid this they need sorted data to see how their company going, they are able to learn their customers, their behaviours , preferances, and needs. That information would be useful for the future business and and business decision making.
+#2. If analyzing and sorting large data manually in Excel is difficult, how do you think Python can help solve this problem?
+#Python is very fast and able to work with large amount of data. It is also greateful work with collecting, ,sorting, analyzing some information, and filtering as well. You can work with just one code in different tables many times automative which saves your time effectively. In python you don't work with ready programm, but you create your own programm which also keeps your motivation in high. It also works with clear code at once, while in excel you would may repeat same formulas for each table.  
+
+#3. Imagine you work at a sales company that receives data about 10,000 customer transactions daily. How would you analyze this data?
+#I will put them into steps, firstle, I would collect data and clean them, then I would sort, catigorise, visualize, and  at the end its easy to anylse it, see trends, and forecast as well
+
+#4. In your opinion, what tasks can Python be useful for in BI processes? (e.g., data cleaning, visualization, etc.)
+#As I mentioned before, python works with data in BI process, it sorts, catigorise, and visualsize soma data.  
+
+#5. If you wanted to compare a company's profit year by year, how could this be done using Python?
+#I will input data into it, ther I will clean it, categorize among each year profit then I can compare data through trend in charts.
+#6. If you don't know Python, what difficulties might you face when working with large datasets?
+#I need to analyse data manually, repeating same formulas many times. I could messed up. Working in vast amount of data take huge amount of time as well.
